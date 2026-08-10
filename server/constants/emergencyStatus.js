5640b50cb043a3a0e734f7e02bc1514fa0a48695
ak8abhinay <abhinay.kakunuri@gmail.com>
2026-08-10 05:47:33 +0000

    emergencyStatus

---
+++ server/constants/emergencyStatus.js
@@ -0,0 +1,8 @@
+export const EmergencyStatus = {
+  WAITING: "waiting",
+  ASSIGNED: "assigned",
+  AT_SCENE: "atScene",
+  AWAITING_HOSPITAL: "awaitingHospital",
+  TRANSPORTING: "transporting",
+  RESOLVED: "resolved",
+};
